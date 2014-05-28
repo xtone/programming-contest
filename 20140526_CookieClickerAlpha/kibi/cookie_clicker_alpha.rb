16616450c14b799668d49b75556be1ae7052e395
kibihara <k.kibihara@xtone.co.jp>
2014-05-28 12:18:47 +0900

    I solved!

---
+++ 20140526_CookieClickerAlpha/kibi/cookie_clicker_alpha.rb
@@ -0,0 +1,48 @@
+class CookieClickerAlpha
+  def initialize(filepath)
+    @input_file_path = filepath
+  end
+
+  def execute
+    File.open @input_file_path, 'r' do |io|
+      # 1行目にテストケースの数が書いてある
+      io.gets.to_i.times do |i|
+        goal_time = solve *io.gets.split(' ').map{ |item| item.to_f }
+        output i+1, goal_time
+      end
+    end
+  end
+
+  # 問題を解く
+  # @param [Float] c クッキーファームを購入するのに必要なクッキー枚数
+  # @param [Float] f クッキーファームにより加算される毎秒加算クッキー枚数
+  # @param [Float] x 勝利条件のクッキー枚数
+  def solve(c, f, x)
+    gain = 2.0
+    goal_time = 0.0
+    while clearing_condition(c, f, x, gain)
+      goal_time += c / gain
+      gain += f
+    end
+    goal_time += x / gain
+  end
+
+  # クッキーファームを追加する場合としない場合で、クッキー所持数0枚の状態からクリアまでの時間を比較する
+  # @param [Float] c クッキーファームを購入するのに必要なクッキー枚数
+  # @param [Float] f クッキーファームにより加算される毎秒加算クッキー枚数
+  # @param [Float] x 勝利条件のクッキー枚数
+  # @param [Float] gain 1秒あたりに得られるクッキー枚数
+  # @return [Boolean] クッキーファームを追加した方がクリア時間が短くなるならtrue
+  def clearing_condition(c, f, x, gain)
+    c / gain + x / (gain + f) < x / gain
+  end
+
+  def output(case_id, goal_time)
+    print "Case ##{case_id}: #{sprintf "%.7f", goal_time}\n"
+  end
+end
+
+
+
+cca = CookieClickerAlpha.new ARGV[0]
+cca.execute
