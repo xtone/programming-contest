7cf52cab0109cc1defa63bb22c1d8b7ea4d29ebe
rswisteria <1cunmu56>
2014-09-29 20:24:33 +0900

    入力箇所を実装。

--- 20140916_CheckerboardMatrix/toyota/checker.rb
+++ 20140916_CheckerboardMatrix/toyota/checker.rb
@@ -1,4 +1,6 @@
 class Matrix
+  attr_accessor :matrix
+
   def initialize(n)
     @n = n
     @matrix = ''
@@ -48,7 +50,34 @@ class Matrix
     end
   end
 
+  def can_solve?
+    row = Array.new @n * 2, ''
+    column = Array.new @n * 2, ''
+    (@n * 2).times do |i|
+      (@n * 2).times do |j|
+        row[i] += self[i, j]
+        column[j] += self[i, j]
+      end
+    end
+    if row.uniq.length != 2 then
+      return false
+    elsif column.uniq.length != 2 then
+      return false
+    end
+    true
+  end
+
+  def solve(case_number)
+    unless can_solve? then
+      puts "Case ##{case_number}: IMPOSSIBLE"
+      return false
+    end
+    puts "Case ##{case_number}: "
+    return true
+  end
+
   def self.goal_matrix(n, start=0)
+    m = Matrix.new(n)
     matrix = ''
     current = start
     (n*2).times do |i|
@@ -56,7 +85,64 @@ class Matrix
         matrix << current.to_s
         current = current == 0 ? 1 : 0
       end
+      m.update(matrix)
+      matrix = ''
+      current = current == 0 ? 1 : 0
     end
-    matrix
+    m
   end
 end
+
+require 'pp'
+
+class SampleReader
+  attr_accessor :case_complete, :matrix, :case
+
+  def initialize
+    @status = :q_count
+  end
+
+  def add_line(line)
+    case @status
+    when :q_count
+      @question_count = line.to_i
+      @case = 0
+      @all_complete = false
+      @case_complete = false
+      @status = :line_count
+    when :line_count
+      @matrix = Matrix.new(line.to_i)
+      @case += 1
+      @status = :line
+      @case_complete = false
+      @rest_line = line.to_i * 2
+    when :line
+      @matrix.update(line.strip)
+      @rest_line -= 1
+      if @rest_line <= 0 then
+        @case_complete = true
+        if @question_count == @case
+          @status = :finished
+          @all_complete = true
+        else
+          @status = :line_count
+        end
+      end
+    end
+  end
+end
+
+filename = ARGV[0]
+reader = SampleReader.new
+File::open(filename) { |file|
+  impossible_count = 0
+  file.each { |line|
+    reader.add_line(line)
+    if reader.case_complete then
+      unless reader.matrix.solve(reader.case) then
+        impossible_count += 1
+      end
+    end
+  }
+  puts "impossible_count: #{impossible_count}"
+}
